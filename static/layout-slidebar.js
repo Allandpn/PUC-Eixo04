a91bc2f21f3bbd0860b88fe333a675d9b08cb582
Allandpn <99135710+Allandpn@users.noreply.github.com>
2023-09-09 20:52:39 -0300

    fixed

---
+++ static/layout-slidebar.js
@@ -0,0 +1,74 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+  const showNavbar = (toggleId, navId, bodyId, headerId) => {
+    const toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId),
+      bodypd = document.getElementById(bodyId),
+      headerpd = document.getElementById(headerId)
+
+    // Validate that all variables exist
+    if (toggle && nav && bodypd && headerpd) {
+      toggle.addEventListener('click', () => {
+        // show navbar
+        nav.classList.toggle('show-navbar')
+        // change icon
+        toggle.classList.toggle('bx-x')
+        // add padding to body
+        bodypd.classList.toggle('body-pd')
+        // add padding to header
+        headerpd.classList.toggle('body-pd')
+        // exibe versao do sistema
+        $("#id-system-version").toggle();
+      })
+    }    
+  }
+
+  showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
+
+  /*===== LINK ACTIVE =====*/
+  const linkColor = document.querySelectorAll('.nav_link')
+
+  function colorLink() {
+    if (linkColor) {
+      linkColor.forEach(l => l.classList.remove('active'))
+      this.classList.add('active')
+    }
+  }
+  linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+
+
+  // Your code to run since DOM is loaded and ready
+});
+
+
+
+
+var myLink = document.querySelector('a[href="#"]');
+myLink.addEventListener('click', function (e) {
+  e.preventDefault();
+
+});
+
+
+
+
+// exibe modal com menu perfil
+$(function() {
+  $(".perfil-icon").click(function(e) {
+    e.preventDefault();
+    el = $(this).data('element');
+    $(el).toggle();
+  });
+});
+
+
+
+// exibe modal com cadastro escola
+$(function() {
+  $(".btn-add-escola").click(function(e) {
+    e.preventDefault();
+    el = $(this).data('element');
+    $(el).toggle();
+  });
+});
