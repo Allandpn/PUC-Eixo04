6be2c20ab20ec3b861774e3d84893805c5c47bca
nikolasaugusto <101151419+nikolasaugusto@users.noreply.github.com>
2023-09-19 18:13:24 -0300

    acervo digital

--- static/layout-slidebar.js
+++ static/layout-slidebar.js
@@ -118,3 +118,21 @@ $(function () {
     $(el).toggle();
   });
 });
+
+// exibe modal com cadastro instrumento
+$(function () {
+  $(".btn-add-instrumento").click(function (e) {
+    e.preventDefault();
+    el = $(this).data('element');
+    $(el).toggle();
+  });
+});
+
+// exibe modal com edição instrumento
+$(function () {
+  $(".btn-edit-instrumento").click(function (e) {
+    e.preventDefault();
+    el = $(this).data('element');
+    $(el).toggle();
+  });
+});
