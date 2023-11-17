54dbf2a40092ec42f71e05f9152e47e827211571
rapa2302 <rafael.alves.587605@sga.pucminas.br>
2023-11-16 21:56:28 -0300

    criação de novo bd para ajustes

---
+++ server/prisma/migrations/20231116002037_novo_build/migration.sql
@@ -0,0 +1,194 @@
+-- CreateTable
+CREATE TABLE "admins" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "dataNascimento" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "senha" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "alunos" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "dataNascimento" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "dataAdmissao" DATETIME,
+    "dataDesligamento" DATETIME,
+    "nomeResponsavel" TEXT,
+    "telefoneResponsavel" TEXT,
+    "emailResponsavel" TEXT,
+    "anotacoesAluno" TEXT,
+    "turmaId" INTEGER,
+    CONSTRAINT "alunos_turmaId_fkey" FOREIGN KEY ("turmaId") REFERENCES "turmas" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "coordenadores" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "dataNascimento" DATETIME NOT NULL,
+    "salario" DECIMAL,
+    "created_at" DATETIME DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME
+);
+
+-- CreateTable
+CREATE TABLE "tiposdeinstrumentos" (
+    "nomeInstrumento" TEXT NOT NULL PRIMARY KEY
+);
+
+-- CreateTable
+CREATE TABLE "instrumentos" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nomeInstrumento" TEXT NOT NULL,
+    "marcaInstrumento" TEXT NOT NULL,
+    "estadoConservacaoDoInstrumento" TEXT NOT NULL,
+    "isEmprestado" BOOLEAN NOT NULL DEFAULT false,
+    "unidadeId" INTEGER NOT NULL,
+    CONSTRAINT "instrumentos_unidadeId_fkey" FOREIGN KEY ("unidadeId") REFERENCES "unidades" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "emprestimoinstrumento" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "instrumentoId" INTEGER NOT NULL,
+    "alunoId" INTEGER NOT NULL,
+    "dataInicialEmprestimo" DATETIME DEFAULT CURRENT_TIMESTAMP,
+    "dataFinalEmprestimo" DATETIME,
+    CONSTRAINT "emprestimoinstrumento_instrumentoId_fkey" FOREIGN KEY ("instrumentoId") REFERENCES "instrumentos" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "emprestimoinstrumento_alunoId_fkey" FOREIGN KEY ("alunoId") REFERENCES "alunos" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "cursos" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nomeCurso" TEXT NOT NULL,
+    "instrumentosCursoNome" TEXT,
+    CONSTRAINT "cursos_instrumentosCursoNome_fkey" FOREIGN KEY ("instrumentosCursoNome") REFERENCES "tiposdeinstrumentos" ("nomeInstrumento") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "turmas" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nroAlunos" INTEGER DEFAULT 0,
+    "nome" TEXT NOT NULL,
+    "isAtiva" BOOLEAN,
+    "horario" TEXT NOT NULL,
+    "cursoId" INTEGER NOT NULL,
+    "coordenadorId" INTEGER,
+    "unidadeId" INTEGER NOT NULL,
+    CONSTRAINT "turmas_cursoId_fkey" FOREIGN KEY ("cursoId") REFERENCES "cursos" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "turmas_coordenadorId_fkey" FOREIGN KEY ("coordenadorId") REFERENCES "coordenadores" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "turmas_unidadeId_fkey" FOREIGN KEY ("unidadeId") REFERENCES "unidades" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "unidades" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "diasdasemana" (
+    "diaDaSemanaInt" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "diaDaSemanaString" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "enderecos" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "rua" TEXT NOT NULL,
+    "numero" TEXT NOT NULL,
+    "complemento" TEXT,
+    "bairro" TEXT,
+    "cidade" TEXT NOT NULL,
+    "CEP" TEXT NOT NULL,
+    "unidadeId" INTEGER,
+    "alunoId" INTEGER,
+    "coordenadorId" INTEGER,
+    CONSTRAINT "enderecos_unidadeId_fkey" FOREIGN KEY ("unidadeId") REFERENCES "unidades" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "enderecos_alunoId_fkey" FOREIGN KEY ("alunoId") REFERENCES "alunos" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "enderecos_coordenadorId_fkey" FOREIGN KEY ("coordenadorId") REFERENCES "coordenadores" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "contribuicoes" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "dataContribuicao" DATETIME DEFAULT CURRENT_TIMESTAMP,
+    "valorContribuicao" DECIMAL NOT NULL,
+    "alunoId" INTEGER NOT NULL,
+    CONSTRAINT "contribuicoes_alunoId_fkey" FOREIGN KEY ("alunoId") REFERENCES "alunos" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_CoordenadorToTiposDeInstrumento" (
+    "A" INTEGER NOT NULL,
+    "B" TEXT NOT NULL,
+    CONSTRAINT "_CoordenadorToTiposDeInstrumento_A_fkey" FOREIGN KEY ("A") REFERENCES "coordenadores" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_CoordenadorToTiposDeInstrumento_B_fkey" FOREIGN KEY ("B") REFERENCES "tiposdeinstrumentos" ("nomeInstrumento") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_DiaDaSemanaToTurma" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+    CONSTRAINT "_DiaDaSemanaToTurma_A_fkey" FOREIGN KEY ("A") REFERENCES "diasdasemana" ("diaDaSemanaInt") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_DiaDaSemanaToTurma_B_fkey" FOREIGN KEY ("B") REFERENCES "turmas" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admins_email_key" ON "admins"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "alunos_email_key" ON "alunos"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "coordenadores_email_key" ON "coordenadores"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tiposdeinstrumentos_nomeInstrumento_key" ON "tiposdeinstrumentos"("nomeInstrumento");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "cursos_nomeCurso_key" ON "cursos"("nomeCurso");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "turmas_nome_key" ON "turmas"("nome");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "unidades_nome_key" ON "unidades"("nome");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "diasdasemana_diaDaSemanaInt_key" ON "diasdasemana"("diaDaSemanaInt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "diasdasemana_diaDaSemanaString_key" ON "diasdasemana"("diaDaSemanaString");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "enderecos_unidadeId_key" ON "enderecos"("unidadeId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "enderecos_alunoId_key" ON "enderecos"("alunoId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "enderecos_coordenadorId_key" ON "enderecos"("coordenadorId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CoordenadorToTiposDeInstrumento_AB_unique" ON "_CoordenadorToTiposDeInstrumento"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CoordenadorToTiposDeInstrumento_B_index" ON "_CoordenadorToTiposDeInstrumento"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_DiaDaSemanaToTurma_AB_unique" ON "_DiaDaSemanaToTurma"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_DiaDaSemanaToTurma_B_index" ON "_DiaDaSemanaToTurma"("B");
