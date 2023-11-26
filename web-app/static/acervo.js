991a94bd30dae204572dc21837b6c5adb9f2f6f7
Allandpn <99135710+Allandpn@users.noreply.github.com>
2023-11-26 18:40:34 -0300

    feito insercao dos dados na pagina do front end

--- web-app/static/acervo.js
+++ web-app/static/acervo.js
@@ -1,73 +1,60 @@
-import { getInstrumentosComEmprestimos } from "../services/instrumentosAPI";
+import { getInstrumentosComEmprestimos } from "./instrumentosAPI.js";
 
-//Teste
 
-//const alunoId = await getAlunoId(1);
 
-const fetchData = async () => {
+async function fetchData() {
   try {
-    const instrumentosComEmprestimos = await getInstrumentosComEmprestimos();
-    console.log(JSON.stringify(instrumentosComEmprestimos));
+    const data = await getInstrumentosComEmprestimos();
+    return data;
   } catch (error) {
     console.error("Error fetching instrumentos", error);
   }
-};
-
-fetchData();
-
-function populaTabela() {
-  // /*html*/
-  // tabela = `<table class="table table-hover">
-  // <thead class="bg-gray">
-  //     <tr>
-  //         <th scope="col"><span class="mr-3 ">Cód.</span><a href="#" ><i class="fa fa-sm fa-sort text-muted" aria-hidden="true"></i></a></th>
-  //         <th scope="col"><span class="mr-3 ">Instrumento</span><a href="#" ><i class="fa fa-sm fa-sort text-muted" aria-hidden="true"></i></a></th>
-  //         <th scope="col"><span class="mr-3 ">Marca</span><a href="#"><i class="fa fa-sm fa-sort text-muted mr-0" aria-hidden="true"></i></a></th>
-  //         <th scope="col"><span class="mr-3 ">Data Aquisição</span><a href="#"><i class="fa fa-sm fa-sort text-muted" aria-hidden="true"></i></a></th>
-  //         <th scope="col"><span class="mr-3 ">Empréstimo</span><a href="#"><i class="fa fa-sm fa-sort text-muted" aria-hidden="true"></i></a></th>
-  //         <th scope="col"><span class="mr-3 ">Responsável</span><a href="#"><i class="fa fa-sm fa-sort text-muted" aria-hidden="true"></i></a></th>
-  //         <th scope="col"><span class="mr-3 ">Data Empr.</span><a href="#"><i class="fa fa-sm fa-sort text-muted" aria-hidden="true"></i></a></th>
-  //         <th scope="col"><span class="mr-3 ">Info</span><a href="#"><i class="fa fa-sm fa-sort text-muted" aria-hidden="true"></i></a></th>
-  //     </tr>
-  // </thead>
-  // <tbody>`
-  // /*html*/
-  // tabela += `         <tr>
-  //                         <td>vlo01</td>
-  //                         <td>Violão</td>
-  //                         <td>Serenata</td>
-  //                         <td>12/05/21</td>
-  //                         <td ><i class="fa fa-check text-info" aria-hidden="true"></i></td>
-  //                         <td>Leide Nunes</td>
-  //                         <td>12/05/22</td>
-  //                         <td>
-  //                             <a href="#" class="open-info-aluno toogle-hide ml-auto" data-element="#dataInfoInstrumento"><i class='bx bx-file-find text-info mt-auto' style="font-size: 1.75rem"></i></a>
-  //                         </td>
-  //                     </tr>
-  //                     <tr>
-  //                         <td>vln01</td>
-  //                         <td>Violino</td>
-  //                         <td>Serenata</td>
-  //                         <td>12/05/22</td>
-  //                         <td>-</td>
-  //                         <td>-</td>
-  //                         <td>-</td>
-  //                         <td>
-  //                             <a href="#" class="open-info-aluno toogle-hide ml-auto" data-element="#dataInfoInstrumento"><i class='bx bx-file-find text-info mt-auto' style="font-size: 1.75rem"></i></a>
-  //                         </td>
-  //                     </tr>
-  //                     <tr>
-  //                         <td>sxf01</td>
-  //                         <td>Saxofone</td>
-  //                         <td>Serenata</td>
-  //                         <td>12/05/23</td>
-  //                         <td><i class="fa fa-check text-info" aria-hidden="true"></i></td>
-  //                         <td>Caique Brito</td>
-  //                         <td>12/05/22</td>
-  //                         <td>
-  //                             <a href="#" class="open-info-instrumento toogle-hide ml-auto" data-element="#dataInfoInstrumento"><i class='bx bx-file-find text-info mt-auto' style="font-size: 1.75rem"></i></a>
-  //                         </td>
-  //                     </tr>
-  //                 </tbody>
-  //             </table>`
 }
+
+async function GetDataAndPopulateTabel() {
+  try {
+    const result = await fetchData();
+    PopulateTable(result);
+  } catch (error) {
+    console.error("Error get data and populate table", error);
+  }
+}
+
+GetDataAndPopulateTabel();
+
+function PopulateTable(dados) {
+  console.log(dados);
+  var tabela = document.querySelector("#tabela-tbody-main");
+
+  var registro = ""; // Move the initialization here
+  
+  for (let i of dados) {
+
+    var isEmprestimo = "";
+      if(i.isEmprestado == true){
+        isEmprestimo = "<i class='fa fa-check text-info' aria-hidden='true'></i>"}
+      else{
+        isEmprestimo =  " - "
+      }
+    
+  registro += `
+    <tr>                            
+      <td>${i.id}</td>
+      <td>${i.nomeInstrumento}</td>                              
+      <td>${i.marcaInstrumento}</td>                             
+      <td>-</td>
+      <td >${isEmprestimo}</td> 
+      <td>Leide Nunes</td>
+      <td> - </td>                          
+      <td> 
+          <a href="#" class="open-info-aluno toogle-hide ml-auto" data-element="#dataInfoInstrumento" value="${i.id}"><i class='bx bx-file-find text-info mt-auto' style="font-size: 1.75rem"></i></a>                                 
+      </td>
+    </tr>
+    `
+
+  }
+   
+    tabela.innerHTML = registro;
+
+}
+
