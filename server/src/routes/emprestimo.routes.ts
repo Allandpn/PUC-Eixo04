1a1ff4341b65cbff9a851023851a6fea9425ce51
rapa2302 <rafael.alves.587605@sga.pucminas.br>
2023-12-03 17:45:04 -0300

    função de devolução de emprestimo

--- server/src/routes/emprestimo.routes.ts
+++ server/src/routes/emprestimo.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { CreateEmprestimoController } from "../modules/emprestimo/useCases/CreateEmprestimoController"
+import { UpdateEmprestimoController } from "../modules/emprestimo/useCases/UpdateEmprestimoController";
 
 
 const createEmprestimoController = new CreateEmprestimoController();
+const updateEmprestimoController = new UpdateEmprestimoController();
 
 const emprestimoRoutes = Router();
 
 emprestimoRoutes.post("/", createEmprestimoController.handle)
+emprestimoRoutes.patch("/", updateEmprestimoController.handle)
 
 export { emprestimoRoutes }
