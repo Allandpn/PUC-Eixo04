d51e0d43ace8fab9e58dcaef8fd35ffecfa312f1
rapa2302 <rafael.alves.587605@sga.pucminas.br>
2023-12-04 11:15:15 -0300

    Criação de API de pegar instrumento por id, ajustes na lógica de negócio de empréstimo de instrumento

--- server/src/routes/instrumento.routes.ts
+++ server/src/routes/instrumento.routes.ts
@@ -2,15 +2,19 @@ import { Router } from "express";
 import { CreateInstrumentoController } from "../modules/instrumento/useCases/createInstrumentoController";
 import { GetInstrumentoController } from "../modules/instrumento/useCases/getInstrumentoController";
 import { GetInstrumentoComEmprestimoController } from "../modules/instrumento/useCases/getInstrumentoComEmprestimoController";
+import { GetInstrumentoIdController } from "../modules/instrumento/useCases/getInstrumentoIdController";
 
 const createInstrumentoController = new CreateInstrumentoController();
 const getInstrumentoController = new GetInstrumentoController();
 const getInstrumentoComEmprestimoController = new GetInstrumentoComEmprestimoController();
+const getInstrumentoIdController = new GetInstrumentoIdController()
 
 const instrumentoRoutes = Router();
 
 instrumentoRoutes.post("/", createInstrumentoController.handle);
-instrumentoRoutes.get("/", getInstrumentoController.handle);
 instrumentoRoutes.get("/emprestimo", getInstrumentoComEmprestimoController.handle);
+instrumentoRoutes.get("/", getInstrumentoController.handle);
+instrumentoRoutes.get("/:id", getInstrumentoIdController.handle);
+
 
 export { instrumentoRoutes };
